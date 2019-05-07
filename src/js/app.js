5c3898e85d9faa8bf1cf1dfdf02c171e144f4323
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-05-07 16:48:25 +0300

    added fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,17 @@ import svg4everybody from 'svg4everybody';
 import './common';
 import { BODY, NO_TOUCH } from './constants';
 import { isTouch } from './utils';
+import fancybox from '@fancyapps/fancybox';
+$('[data-fancybox]').fancybox({
+  protect 		  : true,
+  keyboard        : true,
+  animationEffect : 'zoom',
+  arrows          : true,
+  clickContent    : false,
+  buttons: [
+	  'close'
+  ]
+});
 svg4everybody();
 
 if (!isTouch()) BODY.addClass(NO_TOUCH);
